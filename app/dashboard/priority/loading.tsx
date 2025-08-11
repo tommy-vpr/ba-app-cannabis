1a726865d291af2fc57eda35c00160dd7afee6d0
tommy-vpr <tommy@cultivatedagency.com>
2025-08-11 12:58:12 -0700

    added skeleton to priority loading

---
+++ app/dashboard/priority/loading.tsx
@@ -0,0 +1,8 @@
+import { SkeletonCompanyList } from "@/app/components/skeleton/SkeletonCompanyList";
+import React from "react";
+
+const loading = () => {
+  return <SkeletonCompanyList />;
+};
+
+export default loading;
